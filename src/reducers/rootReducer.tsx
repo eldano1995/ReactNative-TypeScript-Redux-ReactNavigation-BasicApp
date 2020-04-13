b384f8477e737229ffcd2f1ae5a719f665b4c517
unknown <eldano1995@gmail.com>
2020-04-13 13:16:51 -0400

    First commit

---
+++ src/reducers/rootReducer.tsx
@@ -0,0 +1,10 @@
+// const rootReducer: any = { text: "Hello World" }
+const rootReducer = (state: any = {}, action: any) => {
+    switch (action.type) {
+        default:{
+            return state
+        }
+    }
+}
+
+export default rootReducer;
